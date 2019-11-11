9fa7e5af58c40647e913e2d5a7a2c2db9b923838
ygorbunkov <root@mn-an-io.astelit.ukr>
2019-11-11 18:57:34 +0200

    shapes are rendered according to the props

--- src/components/ui/playCard.js
+++ src/components/ui/playCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card, CardActionArea } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-import { Diamond, Squiggle, Oval } from './shapes'
+import { Shape } from './shape'
 
 export const PlayCard = props => {
 	
@@ -12,21 +12,13 @@ export const PlayCard = props => {
 				}
 			}),
 			classes = useStyles(),
-			{position, qty, shape, ...colorFill} = props,
-			getShape = (shape, key) => {
-				const shapeMap = {
-					diamond: <Diamond {...colorFill} key={key} />,
-					squiggle: <Squiggle {...colorFill} key={key} />,
-					oval: <Oval {...colorFill} key={key} />,
-				}
-				return shapeMap[shape]
-			}
+			{position, qty, ...shapeColorFill} = props
 	
 	return (
 		<Card position={position}>
 			<CardActionArea className={classes.card}>
 				{
-					Array.from({length: qty}, (_,key) => getShape(shape,key))
+					Array.from({length: qty}, (_,key) => <Shape {...shapeColorFill} key={key} />)
 				}
 			</CardActionArea>
 		</Card>
