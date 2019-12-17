69288c55e0cdad17d728e278fd77de9b0a8742a3
ygorbunkov <ygorbunkov@mn-an-io.astelit.ukr>
2019-12-17 15:32:45 +0200

    production build added

---
+++ build/precache-manifest.755cb52c6b6c3d6b1ee0fccc37248685.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe5007ee869d2964b7f5beb8f24e052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8b3f32ebcb49e629ee1",
+    "url": "/static/css/main.57d537e8.chunk.css"
+  },
+  {
+    "revision": "7c87da472bb56acb9293",
+    "url": "/static/js/2.54a77d53.chunk.js"
+  },
+  {
+    "revision": "a8b3f32ebcb49e629ee1",
+    "url": "/static/js/main.a7f1ff0a.chunk.js"
+  },
+  {
+    "revision": "6cb5dbaaa829e8b1627e",
+    "url": "/static/js/runtime-main.dea2e961.js"
+  },
+  {
+    "revision": "e880aa9b8a171b5f5953dbdda91a9d29",
+    "url": "/static/media/deck.e880aa9b.svg"
+  },
+  {
+    "revision": "c089081a2e901a204aaa2344ebe8c2a5",
+    "url": "/static/media/logo.c089081a.svg"
+  },
+  {
+    "revision": "c4feb91776fbeb14217d0ae7ef2589e9",
+    "url": "/static/media/score.c4feb917.svg"
+  },
+  {
+    "revision": "c6aaaa63e3c724dc33711f900f4750c4",
+    "url": "/static/media/time.c6aaaa63.svg"
+  }
+]);
