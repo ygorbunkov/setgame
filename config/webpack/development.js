0b0f86b9c3ec73e7dd884f4c45ae1ed4085af1ed
ygorbunkov <ygorbunkov@sandbox.sandbox>
2021-02-12 23:00:45 +0200

    Webpack Dev Server is functioning

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,3 +1,4 @@
+const { HtmlWebpackPlugin } = require('./plugins')
 const path = require('path')
 
 module.exports = {
@@ -13,4 +14,11 @@ module.exports = {
     port: 3000
   },
   entry: ["react-hot-loader/patch", "./src/client/index.tsx"],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Set Game (dev)',
+      template: path.resolve(__dirname, '../../public/index.html'),
+      filename: 'index.html'
+    })
+  ]
 };
